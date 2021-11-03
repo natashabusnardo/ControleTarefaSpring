cc821292aa08be993e270bc9b28a679f241c3fdf
natashabusnardo <natashabusnardo@hotmail.com>
2021-11-03 10:51:00 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/capgemini/controletarefa/ControletarefaApplication.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.controletarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControletarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControletarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/capgemini/controletarefa/ControletarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capgemini.controletarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControletarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
